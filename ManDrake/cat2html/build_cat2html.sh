052ea6b05dacbbaf40638492d5be47704c946743
Eric Gallager <egall@gwmail.gwu.edu>
2024-03-04 12:48:09 -0500

    cat2html build updates
    more output for debugging
    also fix cat2html build

--- ManDrake/cat2html/build_cat2html.sh
+++ ManDrake/cat2html/build_cat2html.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+set -ex
+
 lex cat2html.l
-gcc -O3 lex.cat2html.c -o cat2html
+gcc -O3 -Wall -Wextra -Wno-sign-compare -pedantic lex.cat2html.c -o cat2html
 rm lex.cat2html.c
 strip -x cat2html
 chmod +x cat2html
